2a6c8946eb1f79317717cb8b9474ff3959547d02
Otte <128637052+Otte-S@users.noreply.github.com>
2026-03-05 16:43:35 +0100

    Rename 'wemx_sso_secret' to 'sso_secret'

--- src/config/sso.php
+++ src/config/sso.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'wemx_sso_secret' => env('SSO_SECRET'),
+    'sso_secret' => env('SSO_SECRET'),
     'token_length' => 48,
     'token_lifetime' => 60 // lifetime of a token in seconds
 ];
